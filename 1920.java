33db126c7d3c6d9740d44b0906177ac67c68376c
SeongyunChoi <80454348+csy100886@users.noreply.github.com>
2024-04-03 00:07:48 +0900

    Update 1920.java

--- 1920.java
+++ 1920.java
@@ -5,30 +5,37 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int [] A = new int [N];
 
         for(int i = 0; i < N; i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(A);
+
         int M = Integer.parseInt(br.readLine());
-        int [] B = new int[M];
+
         StringTokenizer st1 = new StringTokenizer(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+
         for(int i = 0; i < M; i++){
-            B[i] = Integer.parseInt(st1.nextToken());
-        }
-        for(int i = 0; i < N; i++){
-            int cnt = 0;
-            for(int j = 0; j < M; j++){
-                if(A[j] == B[i]){
-                    cnt++;
+            if(Arrays.binarySearch(A, Integer.parseInt(st1.nextToken())) >= 0){
 
-                }
-            }System.out.println(cnt);
+                sb.append(1).append('\n');
+
+            }
+            else {
+                sb.append(0).append('\n');
+            }
 
         }
+        System.out.println(sb);
 
 
     }
